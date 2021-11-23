e40f026d617a469f1518c9dbfd90b1b6395d53ea
yosafatraditya <yospan.in.itb@gmail.com>
2021-11-23 21:11:10 +0700

    added adt buff

---
+++ src/ADT/buff.h
@@ -0,0 +1,33 @@
+#ifndef buff_H
+#define buff_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "listlinier.h"
+#include "player.h"
+#include "boolean.h"
+
+typedef struct {
+boolean isActive[4];
+} Buff;
+
+/* LIST BUFF
+    1. Imunitas teleport
+    2. Cermin pengganda
+    3. Senter pembesar hoki
+    4. Senter pengecil hoki
+*/
+
+/* Selektor */
+#define BUFF(B) (B).isActive
+
+/******************************* Buff *******************************/
+
+void printBuff ();
+/* mengeluarkan list Buff yang dimiliki player */
+
+void resetBuff (Buff b);
+/* me-reset seluruh buff kecuali imunitas teleport setiap ronde
+(karena buff hanya berlaku sekali di suatu ronde) */
+
+#endif
