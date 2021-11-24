b4759873aad877c5c69db0514a67c4b0551ac789
riandyhasan <riandyhsn@gmail.com>
2021-11-24 23:37:38 +0700

    move skill and buff to player

--- src/ADT/player.c
+++ src/ADT/player.c
@@ -4,16 +4,200 @@
 
 void CreatePlayer(Player *p, char name[50], int id){
     List skill;
-    Buff buff;
+    boolean buff[4];
     CreateEmptyList(&skill);
     resetBuff(&buff);
     INFOPLAYER(*p) = id;
     NAME(*p) = name[50];
     INFOSKILL(*p) = skill;
-    INFOBUFF(*p) = buff;
 }
 
 
 boolean isSamePlayer(Player p1, Player p2){
     return INFOPLAYER(p1) == INFOPLAYER(p2) && NAME(p1) == NAME(p2);
+}
+
+/******************************* BUFF *******************************/
+
+void printBuff (boolean b[4]) {
+/* mengeluarkan list Buff yang dimiliki player */
+    for (int i = 0; i < 4; i++) {
+        if (b[0]) {
+            printf("Anda mendapatkan buff imunitas teleport. Anda tidak akan kemana-mana.");
+        }
+        else if (b[1]) {
+            printf("Anda mendapatkan buff cermin pengganda. Selamat, anda bisa mendapatkan 2 skill baru!");
+        }
+        else if (b[2]) {
+            printf("Anda mendapatkan buff senter pembesar hoki. Selamat, hoki anda jadi besar!");
+        }
+        else if (b[3]) {
+            printf("Anda mendapatkan buff senter pengecil hoki. Anda kurang beruntung, Player.");
+        }
+    }
+}
+
+void resetBuff (boolean (*b)[4]) {
+/* me-reset seluruh buff kecuali imunitas teleport setiap ronde
+(karena buff hanya berlaku sekali di suatu ronde) */
+    for (int i = 0; i < 4; i++) {
+        (*b)[i] = false;
+    }
+}
+
+/****************** SKILL ******************/
+
+int printRandoms(int lower, int upper, int count)
+{
+    int i, num;
+
+    srand(time(0));
+
+    for (i = 0; i < count; i++) {
+        num = (rand() % (upper - lower + 1)) + lower;
+    }
+
+    return num ;
+}
+
+
+int GetSkills (){
+
+    /* 
+    1 : Pintu Ga Ke Mana Mana
+    2 : Cermin Pengganda
+    3 : Senter Pembesar Hoki
+    4 : Senter Pengecil Hoki
+    5 : Mesin Penukar Posisi
+    6 : Teknologi Gagal*/
+
+    int TabSkill[100];
+    int i, SkillDrop, SkillGet ;
+
+    i = 0;
+
+    while (i < 100){
+
+        if ((i >= 0) && (i <= 15)){
+            TabSkill[i] = 1 ;
+        }
+        else if ((i >= 16) && (i <= 23)){
+            TabSkill[i] = 2 ;
+        }
+        else if ((i >= 24) && (i <= 43)){
+            TabSkill[i] = 3 ;
+        }
+        else if ((i >= 44) && (i <= 63)){
+            TabSkill[i] = 4 ;
+        }
+        else if ((i >= 64) && (i <= 68)){
+            TabSkill[i] = 5 ;
+        }
+        else if ((i >= 69) && (i <= 99)){
+            TabSkill[i] = 6 ;
+        }
+
+        i++ ;
+    }
+
+    SkillDrop = printRandoms(0, 100, 1);
+    SkillGet = TabSkill[SkillDrop];
+
+    return SkillGet;
+}
+
+void insPlayerSkill (Player *P){
+
+    int X ;
+
+    X = GetSkills() ;
+
+    if (X == 6){
+        /*DO NOTHING*/
+        printf("Maaf, anda kurang beruntung :D\n");
+    }
+    else{
+        InsVLast (&INFOSKILL(*P), X);
+
+        if (X == 1){
+            printf("Pintu Ga Ke Mana Mana berhasil dimasukkan ke dalam list!\n");
+        }
+        else if (X == 2){
+            printf("Cermin Pengganda berhasil dimasukkan ke dalam list!\n");
+        }
+        else if (X == 3){
+            printf("Senter Pembesar Hoki berhasil dimasukkan ke dalam list!\n");
+        }
+        else if (X == 4){
+            printf("Senter Pengecil Hoki berhasil dimasukkan ke dalam list!\n");
+        }
+        else if (X == 5){
+            printf("Mesin Penukar Posisi berhasil dimasukkan ke dalam list!\n");
+        }
+    }
+}
+
+
+/****************** PROSES SKILL ******************/
+
+void PrintSkill (Player P){
+/* mengeluarkan list skill yang dimiliki player */
+
+    int i;
+    address T;
+
+    T = First(INFOSKILL(P));
+    i = 1;
+
+    if (T == Nil){
+        printf("Kamu gapunya skill apa apa, Kasian banget :(\n");
+    }
+    else{
+
+        while (T != Nil){
+            printf("%d. ", i);
+
+            if (Info_Skill(T) == 1){
+                printf("Pintu Ga Ke Mana Mana\n");
+            }
+            else if (Info_Skill(T) == 2){
+                printf("Cermin Pengganda\n");
+            }
+            else if (Info_Skill(T) == 3){
+                printf("Senter Pembesar Hoki\n");
+            }
+            else if (Info_Skill(T) == 4){
+                printf("Senter Pengecil Hoki\n");
+            }
+            else if (Info_Skill(T) == 5){
+                printf("Mesin Penukar Posisi\n");
+            }
+
+            T = Next(T);
+            i++ ;
+
+            printf("\n");
+        }
+    }
+
+    printf("Tekan 0 untuk keluar. Masukkan bilangan negatif untuk membuang skill.");
+}
+
+void DoubleMirror(Player *P){
+    
+    List X;
+    int i;
+
+    X = INFOSKILL(*P);
+
+    if (NbElmt(X) < 9){
+        DelP(&X, 2);
+
+        for (i=0; i=1; i++){
+            insPlayerSkill(P);
+        }
+    }
+    else{
+        printf("Kamu tidak memiliki space cukup untuk skill ini. Skill terhapus dan kamu tidak mendapat apa apa !\n");
+    }
 }
--- src/ADT/player.h
+++ src/ADT/player.h
@@ -2,8 +2,6 @@
 #ifndef _PEMAIN_H
 #define _PEMAIN_H
 #include "../boolean.h"
-#include "listlinier.h"
-#include "buff.h"
 #include <stdlib.h>
 
 /* Deklarasi type elemen */
@@ -11,17 +9,70 @@ typedef struct {
 int playernum;
 char name;
 List skill;
-Buff buff;
+boolean buff[4];
 } Player;
 
 /* Selektor */
 #define INFOPLAYER(p) (p).playernum
 #define INFOSKILL(p) (p).skill
-#define INFOBUFF(p) (p).buff
 #define NAME(p) (p).name
 
 void CreatePlayer(Player *p, char name[50], int id);
 
 boolean isSamePlayer(Player p1, Player p2);
+
+
+/******************************* BUFF *******************************/
+
+/* LIST BUFF
+    1. Imunitas teleport
+    2. Cermin pengganda
+    3. Senter pembesar hoki
+    4. Senter pengecil hoki
+*/
+
+void printBuff (boolean b[4]);
+/* mengeluarkan list Buff yang dimiliki player */
+
+void resetBuff (boolean (*b)[4]);
+/* me-reset seluruh buff kecuali imunitas teleport setiap ronde
+(karena buff hanya berlaku sekali di suatu ronde) */
+
+/****************** SKILL ******************/
+
+int printRandoms (int lower, int upper, int count);
+/* I.S. Batas atas dan batas bawah serta jumlah output yang diinginkan terdefinisi */
+
+int GetSkills ();
+/* Mengirimkan hasil skill yang didapat menggunakan randomizer */
+
+void insPlayerSkill (Player *P);
+/* I.S. Skill Player mungkin kosong  */
+/* F.S. Skill baru ditambahkan ke dalam list skill player */
+
+/******************************* PROSES SKILL *******************************/
+
+void PrintSkill ();
+/* I.S. Sembarang  */
+/* F.S. Mengeluarkan list skill yang dimiliki oleh player */
+
+
+// void CommandSkill (State *S, int id);
+// /* I.S. Sembarang  */
+// /* F.S. mengeluarkan command untuk meminta masukkan skill yang ingin dipakai */
+
+
+// void TukarPosisiPlayer (Player P1, Player P2, State *S);
+// /* I.S. Posisi Player berada pada posisi sebenarnya (posisi aslinya) */
+// /* F.S. Posisi Player 1 berubah menjadi posisi Player 2 dan begitu sebaliknya */
+
+// void UseTukarPosisiPlayer (State *S, int Playernum1);
+// /* I.S. Posisi Player berada pada posisi sebenarnya (posisi aslinya) */
+// /* F.S. Posisi Player 1 berubah menjadi posisi Player 2 dan begitu sebaliknya */
+
+void DoubleMirror(Player *P);
+/* I.S.  terdapat skill Cermin Pengganda di dalam list skillnya.*/
+/* F.S. Skill Cermin Pengganda -> Menghapus dirinya sendiri dan mendapatkan 2 buah skill baru */
+
 
 #endif
