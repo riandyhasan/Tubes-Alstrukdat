82bcf5fc7a3888cf6a6e90340f7b3837c2642f90
alifiyabs <alifiyabshary@gmail.com>
2021-11-26 23:27:46 +0700

    update driverTeleporter.c

--- src/ADT/driverTeleporter.c
+++ src/ADT/driverTeleporter.c
@@ -1,43 +1,40 @@
 #include "Teleporter.h"
 #include "Teleporter.c"
-#include "Skill.h"
-#include "Skill.c"
-#include "state.h"
-#include "state.c"
 #include <stdio.h>
 #include <stdlib.h>
+
+/* DRIVER TELEPORTER.C */
 
 int main () {
     int N,i,addIn,addOut,noPetak;
     arrayTele arrTele;
-    PlayerState ps;
     Teleporter tele;
-
     MakeEmptyTele(&arrTele);
-
-    PetakInAndOut(&tele,addIn,addOut);
-
-    TotalElmtTele(arrTele);
-
-    MaxElmt(arrTele);
-
-    GetFirstIdxTele(arrTele);
-
-    GetLastIdxTele(arrTele);
-
-    IsTeleport(arrTele,noPetak);
-
-    PetakOut(arrTele,noPetak);
-
-    MovePemain(&ps,arrTele);
-
-    PrintTele(arrTele);
-
+    scanf("%d", &N);
+    printf("Silahkan masukkan Petak masuk dan Petak keluar!\n");
+    for (i = 1; i <= N;i++) {
+        scanf("%d %d", &addIn, &addOut);
+        PetakInAndOut(&tele,addIn,addOut);
+        arrTele.bufferTele[i].IdxMasuk = addIn;
+        arrTele.bufferTele[i].IdxKeluar = addOut;
+        arrTele.Neff++;
+    }
     printf("\n");
-
-    CmdInspect(arrTele);
-
+    printf("Total Elemen dalam Array Teleporter adalah %d\n", TotalElmtTele(arrTele));
+    printf("Total maksimal elemen dalam Array Teleporter adalah %d\n", MaxElmtTele(arrTele));
+    printf("Indeks pertama dari Array Teleporter adalah %d\n", GetFirstIdxTele(arrTele));
+    printf("Indeks terakhir dari Array Teleporter adalah %d \n", GetLastIdxTele(arrTele));
     printf("\n");
-
-    printf("Sudah Selesai\n");
+    printf("Masukkan Nomor Petak!\n");
+    scanf("%d", &noPetak);
+    printf("\n");
+    if (IsTeleport(arrTele,noPetak)) {
+        printf("Petak Keluar dari %d adalah %d \n", noPetak, PetakOut(arrTele,noPetak));
+    } else {
+        printf("Petak %d tidak memiliki Teleporter dan Petak Keluar\n", noPetak);
+    }
+    printf("\n");
+    PrintTele(arrTele); /* Menampilkan Petak Teleporter dan Petak Keluar Teleporter */
+    printf("\n");
+    printf("Sudah Selesai yey!\n");
 }
